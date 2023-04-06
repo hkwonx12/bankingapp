6ddafc0d08b3a56ae320be3f0a1567558c43ae66
hkwonx12 <helenkwonx2@gmail.com>
2023-04-06 18:24:29 -0400

    Checking updated with relational database

--- api/routers/checking_account.py
+++ api/routers/checking_account.py
@@ -34,34 +34,33 @@ def get_all_checking_account(
     return repo.get_all_checking_accounts()
 
 
-@router.get('/api/checking_account/{account_number}', response_model=CheckingAccountOutWithDetails)
+@router.get('/api/checking_account/{owner_id}', response_model=CheckingAccountOutWithDetails)
 def get_one_checking_account(
-    account_number: int,
-    # info:CheckingAccountOutWithDetails,
+    owner_id: int,
     response: Response,
     repo: CheckingAccountRepository = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> CheckingAccountOutWithDetails:
-    checking_account = repo.get_one_checking_account(account_number)
+    checking_account = repo.get_one_checking_account(owner_id)
     if checking_account is None:
         response.status_code = 404
     return checking_account
 
 
-@router.delete('/api/checking_account/{account_number}', response_model=bool)
+@router.delete('/api/checking_account/{id}', response_model=bool)
 def delete_checking_account(
-    account_number: int,
+    id: int,
     repo: CheckingAccountRepository = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> bool:
-    return repo.delete_checking_account(account_number)
+    return repo.delete_checking_account(id)
 
 
-@router.put('/api/checking_account/{account_number}', response_model=CheckingAccountOutWithDetails)
+@router.put('/api/checking_account/{id}', response_model=CheckingAccountOutWithDetails)
 def update_checking_account(
-    account_number: int,
+    id: int,
     checking_account: CheckingAccountIn,
     repo: CheckingAccountRepository = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> CheckingAccountOutWithDetails:
-    return repo.update_checking_account(account_number, checking_account)
+    return repo.update_checking_account(id, checking_account)
