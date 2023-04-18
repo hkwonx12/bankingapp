545d08b1db525c82da3c0053b86a098707f99de8
Aaron <cornwell_aaron@yahoo.com>
2023-04-18 16:24:25 -0400

    login and logout modified

--- ghi/src/Login.jsx
+++ ghi/src/Login.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {handlePasswordChange, handleUsernameChange, reset} from "./features/auth/loginSlice"
+import {useLoginMutation} from "./services/users"
 
 const Login = () => {
     const dispatch = useDispatch()
+    const [login] = useLoginMutation()
     const { fields } = useSelector(state => state.login)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log('handleSubmit');
+        login(fields)
         dispatch(reset())
     }
     return (
