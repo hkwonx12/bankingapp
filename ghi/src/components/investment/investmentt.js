ec2e840879aa795d8810097f6340814ad631ab48
hkwonx12 <helenkwonx2@gmail.com>
2023-04-25 11:19:10 -0400

    Investment update

--- ghi/src/components/investment/investmentt.js
+++ ghi/src/components/investment/investmentt.js
@@ -1,37 +1,37 @@
-import { useAuthContext } from '@galvanize-inc/jwtdown-for-react';
-import { useEffect, useState } from 'react';
+// import { useAuthContext } from '@galvanize-inc/jwtdown-for-react';
+// import { useEffect, useState } from 'react';
 
-function InvestmentInformation() {
-    const {token, user} = useAuthContext();
-    const [investmentTotal, setInvestmentTotal] = useState([]);
-    const [stock, setStock] = useState([])
+// function InvestmentInformation() {
+//     const {token, user} = useAuthContext();
+//     const [investmentTotal, setInvestmentTotal] = useState([]);
+//     const [stock, setStock] = useState([])
 
-    const getData = async () => {
-        const response = await fetch(`http://localhost:8000/api/investment_account/${owner_id}`,
-        {headers: {Authorization: `Bearer ${token}`}});
+//     const getData = async () => {
+//         const response = await fetch(`http://localhost:8000/api/investment_account/${owner_id}`,
+//         {headers: {Authorization: `Bearer ${token}`}});
 
-        if (response.ok){
-            const data = await response.json();
-            investmentTotal(data.total_amount)
-        }
-    };
-    const getStockData = async () => {
-        const response = await fetch('http://localhost:8000/api/stock/QQQ')
-        if (response.ok){
-            const stockData = await response.json()
-            setStock(stockData)
-        }
+//         if (response.ok){
+//             const data = await response.json();
+//             investmentTotal(data.total_amount)
+//         }
+//     };
+//     const getStockData = async () => {
+//         const response = await fetch('http://localhost:8000/api/stock/QQQ')
+//         if (response.ok){
+//             const stockData = await response.json()
+//             setStock(stockData)
+//         }
 
-    }
+//     }
 
-    useEffect(() => {
-        if (token) getData();
-        if (user){
-            
-        }
-    }, [token, user]);
-return (
-    <></>
-)
-}
-export default InvestmentInformation
+//     useEffect(() => {
+//         if (token) getData();
+//         if (user){
+
+//         }
+//     }, [token, user]);
+// return (
+//     <></>
+// )
+// }
+// export default InvestmentInformation
